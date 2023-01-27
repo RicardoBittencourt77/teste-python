9ee8a63b99db81b3bcdf12041a88b1f6c3155c2d
Ricardo Bittencourt <ricardo77.bittencourt@gmail.com>
2023-01-27 09:30:58 -0300

    foi acrescentada a mensagem do dia da semana

--- arquivo1.py
+++ arquivo1.py
@@ -1,1 +1,12 @@
-print('Hello world!')
+from datetime import date
+
+sem = ("Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo")
+
+dia_da_semana = date.today().weekday()
+
+if dia_da_semana < 5:
+    diaSemana = (f"{sem[dia_da_semana]}-feira")
+else:
+    diaSemana = (f"{sem[dia_da_semana]}")
+
+print('Olá mundo! Hoje é {}!'.format(diaSemana))
